fc34b62aeb278f1169b355ccb75e15110444e673
Sam <sam3013sam1@outlook.com>
2020-07-24 12:03:38 -0700

    git init

---
+++ src/Model/Employee.java
@@ -0,0 +1,62 @@
+package Model;
+
+
+public class Employee {
+
+    private boolean sex ;
+    private String name ;
+    private int grade ;
+    private int salary ;
+    private int id ;
+
+    Employee (int id , String name , int salary , int grade ) {
+        this.grade = grade ;
+        this.id = id ;
+        this.name = name ;
+        this.salary = salary ;
+    }
+
+
+
+    public void setInfo (int id , String name , int salary ) {
+        this.id = id ;
+        this.name = name ;
+        this.salary = salary ;
+    }
+
+    String getInfo () {
+        return "mr/ ms " + this.name + " with id " + this.id + " has $" + this.salary + " salary." ;
+    }
+
+
+    public void setName (String name) {
+        this.name = name ;
+    }
+
+
+}
+
+
+class TestEmployee {
+    public static void main (String args[]) {
+        Employee Kavian = new Employee (745 , "ali" , 45000 , 7 ) ;
+
+        Kavian.setName("tara");
+
+        Kavian.setInfo (757 , "reza" , 60000 );
+
+        System.out.println(Kavian.getInfo());
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
